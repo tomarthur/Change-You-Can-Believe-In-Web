57ea5650a0852f0308e01caef0850328026bf28a
Tom Arthur <tom.arthur@mac.com>
2012-12-11 02:04:21 -0500

    building out views

--- app.rb
+++ app.rb
@@ -1,72 +1,104 @@
 require 'sinatra'
 require 'dm-core'
+require 'dm-timestamps'
 
-DataMapper::setup(:default, {:adapter => 'yaml', :path => 'db'}) #talk to database yaml (type, could be mysql, to the db folder
+DataMapper::setup(:default, {:adapter => 'yaml', :path => 'db'}) 
+#talk to database yaml (type, could be mysql, to the db folder
 
 class Money
   include DataMapper::Resource
 
-  property :id,   Serial
-  property :time, Integer
-  property :lasttotal, Float # old total
-  property :penny, Integer   # number of pennys
-  property :nickel, Integer  # number of nickels
-  property :dime, Integer	 # number of dimes	
-  property :quarter, Integer  # number of quarters
-  property :total, Float	 # new total
-  
+  property :id,   Serial		  # each record is equal to one coin collection event
+  property :created_at, DateTime
+  property :created_on, Date
+  property :lasttotal, Float
+  property :penny, Integer 		  # number of pennys
+  property :nickel, Integer 	  # number of nickels
+  property :dime, Integer		  # number of dimes	
+  property :quarter, Integer 	  # number of quarters
+  property :total, Float	 	  # new total
+
 end
 
 class Tag
   include DataMapper::Resource
   
-  property :id, Serial
+  property :id, Serial	
+  property :created_at, DateTime
+  property :created_on, Date
+  property :updated_at, DateTime
+  property :updated_on, Date
+  property :numscans, Integer
   property :tagid, Integer		# tag ID used by Arduino
   property :name, String	    # human readable name of item
   property :price, Float		# price of item
   property :giving, Boolean		# TRUE == item for charity, FALSE == item for self
-  property :lastscan, Integer   # Time of last scan by Arduino
-  property :numscans, Integer
   property :img, String			# Image address
-  property :onarduino, Boolean  # FALSE == tag made on web not updated to arduino
-  
+ 
 end
 
+class TagScan
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :created_at, DateTime
+  property :created_on, Date
+  property :tagid, Integer
+  
+end
+  
 DataMapper.finalize
 
-# Main route
+# Homepage
 get '/' do
   erb :home, :layout => false
 end
 
-get '/totals' do
+get '/about' do
 
-  "Connection Success"
+  erb :about
 end
 
-get '/lastcollec' do
+get '/savings' do
 
-  "here's the last collection"
+  erb :savings
 end
 
-get '/analyze' do
+get '/trinkets' do
 
-  "here are statistics about your collection"
+  erb :trinkets
 end
 
-get '/alltags' do
+get '/lastcollection' do
 
-	"hello"
+  erb :lastcollection
 end
 
 get '/newtag' do
 
-  "make a new tag here"
+  erb :newtag
 end
 
-post '/maketag' do
+post '/maketag' do #for adding new RFID tags to the database
 
-  "Make a tag success"
+	@t = Tag.new
+	@t.tagid = params[:tagid]
+	@t.name = params[:name]
+	@t.price = params[:price]
+	@t.giving = params[:giving]
+	@t.img = params[:imgurl]
+	@t.save
+
+    if (@t.save == true)
+	    erb :success
+    else
+	    erb :fail
+    end
+end
+
+get '/success' do
+	
+	"tag was made successfully"
 end
 
 ###############################################################
@@ -76,15 +108,12 @@ end
 
 # http://itp.nyu.edu/~netid/sinatra/yourapp?name=Rune  => params[:name]
 # http://itp.nyu.edu/~tca241/sinatra/change/updatetotal?time=1234&lasttotal=10.00&penny=3&nickel=5&dime=6&quarter=58&total=39.40
+# http://changeitp.herokuapp.com/updatetotal?
+
 
 
 get '/updatetotal' do 		# total sent from arduino after a new collection
-  # params[:yourname] will be replaced with the value entered for 
-  # the input with name 'yourname'  
-
-
   @m = Money.new
-  @m.time = params[:time]
   @m.lasttotal = params[:lasttotal]
   @m.penny = params[:penny]
   @m.nickel = params[:nickel]
@@ -93,20 +122,16 @@ get '/updatetotal' do 		# total sent from arduino after a new collection
   @m.total = @totaltemp
   @m.save
 # raise Exception, @m.save
-    "hurray"
+   "Update Captured"
 end
 
-post '/tagscan' do			# tag scan data sent from arduino
+get '/tagscan' do			# tag scan data sent from arduino
 
-	"Tag Success"
+
+  @s = TagScan.new
+  @s.tagid = params[:tagid]
+  @s.save
+
+  "Tag Captured"
 end
 
-
-	
-###############################################################
-# Arduino future use
-
-get '/newtags' do # send new tags to arduino
-	
-	"new tags sent"
-end
